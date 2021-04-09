18f003336cb4166e07df3533fc801599a7a9e31c
Jennifer Kliber <jklibers@gmail.com>
2021-04-09 17:36:19 -0500

    Updated welcome message

--- bikeshare.py
+++ bikeshare.py
@@ -15,7 +15,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello! Let\'s explore some US bikeshare data from Chicago, NYC or Washington!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     
     city = input("Would you like to see data for Chicago, NYC, or Washington?\n").lower()
